a0af9b3553d056277a2c3914d1fb1813279af15b
zeydinacet <acetzeydin@gmail.com>
2022-12-13 13:01:45 +0100

    ilk degisiklik

--- src/denemegithub/Github_deneme01.java
+++ src/denemegithub/Github_deneme01.java
@@ -3,5 +3,6 @@ package denemegithub;
 public class Github_deneme01 {
     public static void main(String[] args) {
         System.out.println("Merhaba Github");
+        System.out.println("DEneme Github 2");
     }
 }
